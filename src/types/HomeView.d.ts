f3578c243b70fdf5f26ce57c06b1f71a6659a6fc
Werner Gottschalt <wgottschalt.projects@gmail.com>
2025-04-25 10:43:29 -0300

    Project restart: problems with npm version and some packages

--- src/types/HomeView.d.ts
+++
@@ -1,7 +0,0 @@
-import { DefineComponent } from 'vue';
-
-declare module '../views/HomeView.vue' {
-  export default DefineComponent<{
-    // Aqui você pode adicionar propriedades e eventos do componente
-  }>;
-}
--- src/views/HomeView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>{{ $t("home.title") }}</h1>
-</template>
